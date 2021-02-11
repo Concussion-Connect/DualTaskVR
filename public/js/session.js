363abe67300caf93caf38cfcf487cf4de63c4faa
hroweanderson3 <hueymroweanderson3@gmail.com>
2021-02-11 08:02:02 -0500

    Adds some vr compatibility and fixes a bug for receiving videos.

--- public/js/session.js
+++ public/js/session.js
@@ -1,8 +1,18 @@
+// Document Elements
+let videoPlayer = document.getElementById('video-player');
+
 // Global Variables
 const pathArray = window.location.pathname.split('/');
+const screen_width = window.screen.width;
+const screen_orientation = window.screen.orientation;
 const activeCurrentTrial = pathArray[4];
-const states = ["/opening", "/trial1","/trial2", "/trial3", "/trial4",]
+console.log(window.location.host);
+let states = ["/opening", "/trial1","/trial2", "/trial3", "/trial4",]
+if (screen_width < 850) {
+  states = ["/opening_vr", "/trial1_vr","/trial2_vr", "/trial3_vr", "/trial4_vr",]
+} 
 
+// Helper Functions
 export function updateSessionState(id, wordList, currentTrial) {
   if (id == null) {
     location.replace("/");
@@ -15,18 +25,26 @@ export function updateSessionState(id, wordList, currentTrial) {
   }
 }
 
-function endSession() {
-  setTimeout(function() {
-    updateSessionState(null, null, 0);
-    // Update this duration value if the final video is changed
-  }, 19000);
-}
-
 export function sessionHasBeenUpdated(currentTrial) {
   return currentTrial != activeCurrentTrial;
 }
 
+function handleVideoOnEnd() {
+  if (states[activeCurrentTrial] === states[states.length - 1]) {
+    endSession();
+  }
+}
+
+function endSession() {
+  updateSessionState(null, null, 0);
+}
+
 // Main Functionality
-if (states[activeCurrentTrial] === states[states.length - 1]) {
-  endSession();
+if (activeCurrentTrial) {
+  if (screen_width < 850 && screen_orientation.type.includes("portrait")) {
+    videoPlayer.style.transform = "rotate(-90deg)"
+  }
+  videoPlayer.onended = function(){
+    handleVideoOnEnd();
+  }
 }
